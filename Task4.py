f38b641360f9648c65fec183ff7f114d27cabb89
wajishagul <85829819+wajishagul@users.noreply.github.com>
2021-06-16 15:37:39 +0530

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,26 @@
+print("*****Task 4- Variables and Datatype*****")
+print("Excercise")
+print("1. Create three variables (a,b,c) to same value of any integer & do the following")
+a=b=c=100
+print("i.Divide a by 10")
+print(a,"/",10,"=",a/10)
+print("ii.Multiply b by 50")
+print(b,"*",50,"=",b*50)
+print("iii.Add c by 60")
+print(c,"+",50,"=",b+50)
+print("2.Create a String variable of 5 characters and replace the 3rd character with G")
+str="ABCDE"
+print("Length of the string is:",len(str))
+s1="C"
+s2="G"
+print("new string after replacement:",str.replace(s1,s2))
+print("3.Create two values (a,b) of int,float data type & convert the vise versa")
+a,b=200,55.5
+print("a=",a,"b=",b)
+a=float(a)
+b=int(b)
+print("value of a and b after type conversion:")
+print("a=",a,"b=",b)
+print("data type of a after conversion:",type(a))
+print("data type of b after conversion:",type(b))
+print("-----Task 4 Completed-----")
