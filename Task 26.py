bffa90e659f3893e2cb6b20aa07991cca5838740
wajishagul <85829819+wajishagul@users.noreply.github.com>
2021-07-12 18:11:11 +0530

    Add files via upload

---
+++ Task 26.py
@@ -0,0 +1,8 @@
+#Enumerate a python list and try to print the counter with the list value
+list1= ['Sai Ram','jagan','Ramesh','Rajan']
+print(list(enumerate(list1)))
+[(0, 'Sai Ram'), (1, 'jagan'), (2, 'Ramesh'), (3, 'Rajan')]
+print(enumerate(list1))
+#Enumerate a python tuple and try to print the counter with the tuple value
+tuple1=('Sai Ram','jagan','Ramesh','Rajan')
+print(tuple(enumerate(tuple1)))
